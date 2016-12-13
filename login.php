62264072b0a10c37346cf295d51ab4bd12385586
Warlus <g.faggioni5@gmail.com>
2016-12-13 19:24:27 +0100

    change the login, now is able to check even for one type

--- login.php
+++ login.php
@@ -1,40 +1,55 @@
 <?php
 
-require_once '../libs/aifp_controller.php';    
+require_once 'libs/aifp_controller.php';    
 
 function check_post($param){
     $app = array();    
     foreach ($param as $key=>$value){
         if( $key == 'user' ){
             $app[$key] = sanitaze_input($value);
-        }
+        }		
         else if( $key == 'password' ){
             $app[$key] = sanitaze_input($value);
-        }           
+        }
+		else if( $key == 'email' ){
+            $app[$key] = sanitaze_input($value);
+        } 
     }
     return $app; 
 }
-    
-try{
-    $smarty = new AIFP_smarty();
-    $contr = new aifp_controller();
 
-    if(($post = check_post($_POST)) ){
-        $user = $contr->login($pwd, $em);
-        if($user){
-            session_start();
-            $SESSION['user']= $user;           
-        }else{            
-            $smarty->assign('error', $contr->descritpion);
-            $smarty->display('error.tpl');
-        }
-    }
-    $smarty->assign('error', GEN_ERROR);
-    $smarty->display('error.tpl');
+if(isset($_GET['type']){
+	$type = $_GET['type'];
+}else{
+	$type = -1;
+}
 
-} catch (Exception $ex){
+$smarty = new AIFP_smarty();
+$contr = new aifp_controller();
 
-    session_destroy();
-    $smarty->assign('error', $ex->getMessage());
-    $smarty->display('error.tpl');
-}   
+if(($post = check_post($_POST)) ){
+	if(isset($post['password']){
+		$pwd = $post['password'];
+		if(isset($post['email']){
+			$em = $post['email'];
+			$token = $contr->login($pwd, $em, -1, $type);
+		}else if(isset($post['user'])){
+			$us = $post['user'];
+			$token = $contr->login($pwd, -1, $us, $type);
+		if($token){
+			session_start();
+			$_SESSION['user']= $token;           
+		}else{            
+			$smarty->assign('error', $contr->descritpion);
+			$smarty->display('error.tpl');
+		}
+	}else{
+		$smarty->assign('error', GEN_ERROR);
+		$smarty->display('error.tpl');
+	}
+}
+$smarty->assign('error', GEN_ERROR);
+$smarty->display('error.tpl');
+
+
+  
