9062bb7802231d6cd40c1637e7a38c01ed952306
Warlusfag <g.faggioni5@gmail.com>
2017-01-28 00:54:44 +0100

    Change the code, Before you had to pass a parameters. Now you can pass nothing and the system return the image of all species In this way, it is possible access in that page without search nothing before.

--- funghi/schede_funghi.php
+++ funghi/schede_funghi.php
@@ -1,24 +1,29 @@
 <?php
 require_once '../libs/aifp_controller.php';
 
-
 $smarty = new AIFP_Smarty();
-$_GET['genere'] = 'lactarius';
+//$_GET['genere'] = 'lactarius';
 if(isset($_GET['genere'])){
-
+    $genere = $_GET['genere'];
     $contr = new aifp_controller();
     
-    $funghi = $contr->get_scheda_funghi($_GET['genere']);
+    $funghi = $contr->get_scheda_funghi($genere);
     if(!$funghi){
         $smarty->assign('error',$contr->descritpion);
         
     }else{
-        $smarty->assign('funghi',$funghi);
-        $smarty->display('generic_page.tpl');
+        $smarty->assign('genere',$funghi);
     }
-   } 
-
-
+}else{
+    $photo = array();
+    $base_path = './foto_generi/';
+    $files = scandir($base_path);
+    $i = 0;
+    foreach($files as $file){
+        $photo[$i] = $base_path.$file;
+    }
+    $smarty->assign('foto',$photo);
+}
 $smarty->display('schede.tpl');
 
 
