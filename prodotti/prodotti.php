bf0b9ce59c61857673a775ff4c7c150f4259ac75
Warlusfag <g.faggioni5@gmail.com>
2017-02-06 21:52:08 +0100

    arriva il controllore

--- prodotti/prodotti.php
+++ prodotti/prodotti.php
@@ -1,8 +1,24 @@
 <?php
-require_once '../libs/aifp_controller.php';
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+session_start();
 
+require_once '../libs/aifp_controller.php';
+
+$contr = new aifp_controller();
+$smarty = new AIFP_smarty();
+
+if(isset($_POST['tipologia']) ){
+    $tipologia = $_POST['tipologia'];    
+}else{
+    $tipologia = -1;            
+}
+$temp = $contr->get_prodotti($tipologia); 
+if($contr->descritpion != ''){
+    $smarty->assign('error',GEN_ERROR);
+}else{    
+    $smarty->assign('prodotti',$temp);
+}
+if(isset($_SESSION['curr_user'])){
+    $smarty->assign('user',$_SESSION['curr_user']['user']);
+    $smarty->assign('image',$_SESSION['curr_user']['image']);
+}
+$smarty->display('prodotti.tpl');
