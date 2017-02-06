40376ab9d9074ed0378b0b1b10c9845deff213b6
gianmarcotr <gianmarcotroiano@gmail.com>
2017-02-06 17:41:43 +0100

    add schede_piante.php

--- piante/schede_piante.php
+++ piante/schede_piante.php
@@ -1,8 +1,6 @@
 <?php
+session_start();
+require_once '../libs/aifp_controller.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$smarty = new AIFP_Smarty();
+$smarty->display('schede_piante.tpl');
