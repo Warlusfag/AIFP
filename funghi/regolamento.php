771cc9221765756bdbc751a018e1433a6e682f2f
Warlusfag <g.faggioni5@gmail.com>
2017-02-08 22:10:12 +0100

    Regolamento passo solo il path

--- funghi/regolamento.php
+++ funghi/regolamento.php
@@ -1,22 +1,31 @@
 <?php
 session_start();
 require_once '../libs/aifp_controller.php';
-if(isset($_SESSION['curr_user'])){
-    $tok = $_SESSION['curr_user']['token'];    
-    $user = $contr->get_user($tok);
-    
-    $controller = new aifp_controller();
-    
-    $path = $controller->get_regolamento($user->attributes['provincia']);
-    if($path){
-        download($path);
-        $smarty->assign('message',"Il download partirà automaticamente, attendere...");
+
+$controller = new aifp_controller();
+
+if(isset($_GET['regione'])){
+    $path = $controller->get_regolamento($_GET['regione']);
+    if($path){        
+        $smarty->assign('path',$path);
     }else{
         $smarty->assign('error', $controller->description);
     }
+}else if(isset($_GET['miaregione']) && isset($_SESSION['curr_user']) ){
+    $tok = $_SESSION['curr_user']['token'];    
+    $user = $contr->get_user($tok);    
     
+    $path = $controller->get_regolamento($user->attributes['provincia']);
+    if($path){        
+        $smarty->assign('path',$path);
+    }else{
+        $smarty->assign('error', $controller->description);
+    }
+}
+if(isset($_SESSION['curr_user']) ){
     $smarty->assign('user',$_SESSION['curr_user']['user']);
     $smarty->assign('image',$_SESSION['curr_user']['image']);
 }
+
 $smarty = new AIFP_Smarty();
 $smarty->display('regolamento.tpl');
