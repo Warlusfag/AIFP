66e54898c4ce6ac02bd4af90c0580030c8dda1d4
Gianmarco Troiano <gianmarcotroiano@gmail.com>
2017-02-11 19:24:19 +0100

    add eventi.js

---
+++ js/eventi.js
@@ -0,0 +1,23 @@
+
+function testeventi(){
+  var nome = document.forms["s_eventi"]["nome"].value;
+  var regione = document.forms["s_eventi"]["regione"].value;
+  var tipo = document.forms["s_eventi"]["tipologia"].value;
+  var data_inizio = document.forms["s_eventi"]["data_inizio"].value;
+  //if (nome==null && regione==null && tipo==null && data_inizio==null) {
+  if(nome==""){
+    alert("Inserire almeno un campo di ricerca");
+    
+    document.forms["s_eventi"]["regione"].focus();
+
+    document.forms["s_eventi"]["tipologia"].focus();
+    
+    document.forms["s_eventi"]["data_inizio"].focus();
+    document.forms["s_eventi"]["nome"].focus();
+    document.forms["s_eventi"]["nome"].select();
+    return false;
+  }
+return true;  	
+  
+}
+
