554dae05ab4ae2c3b4739deed060314d5e027e0a
Warlus <g.faggioni5@gmail.com>
2017-02-14 13:42:47 +0100

    Upgrade user

--- personal_page/upgrade_user.php
+++ personal_page/upgrade_user.php
@@ -1,38 +1,27 @@
 <?php
 session_start();
 require_once '../libs/aifp_controller.php';
-
 $smarty = new AIFP_smarty();
-
-
-if(isset($_POST['email'])){
+if(isset($_POST['email']) && isset($_POST['type']){
     if(isset($_SESSIONS['curr_user'])){
-        $controller = new aifp_controller();
-        $tok = $_SESSIONS['curr_user']['token'];
+        $controller = new aifp_controller();        
         $type = $_SESSIONS['curr_user']['type'];
-        $ass = $controller->get_user($tok, $type);
-    }else{
-        $ass = -1;    
-    }
-    if($ass instanceof associazione || $ass instanceof admin){
-
-        $email = $_POST['email'];
-        if(!isset($_POST['type'])){
-            $smarty->assign('error', GEN_ERROR);            
-        }else{
-            if($ass->upgrade_user($email, $_POST['type'])){
-                $smarty->assign('message', 'upgrade dell\' utente avvenuto con successo');
+        $ass = $controller->get_us_from_type($type);
+        $ass->init($_SESSIONS['curr_user']);
+        if($ass instanceof associazione || $ass instanceof admin){
+            $email = $_POST['email'];           
+            if($controller->upgrade_user($ass->attributes, $email, $_POST['type'])){
+                $smarty->assign('message', "L'upgrade dell'utente con ".$email." e' avvenuto con successo" );
+                //codice per mandare un email
             }else{
-                $smarty->assign('error', $ass->err_descr);
-            }
+                $smarty->assign('error', $controller->description);
+            }            
+            $smarty->display('personal_page.tpl');
+        }else{
+            $smarty->assign('error', "ERROR: non sei autorizzato ad utilizare questa funzionalita'");
         }
-        $smarty->display('personal_page.tpl');
-    }else{
-        unset($_SESSIONS['curr_user']);
-        $smarty->assign('error', GEN_ERROR);
-        $smarty->display('personal_page.tpl');
     }
 }else{
-    $smarty->assign('error', GEN_ERROR);
-    $smarty->display('personal_page.tpl');    
+    $smarty->assign('error', GEN_ERROR);       
 }
+$smarty->display('personal_page.tpl'); 
